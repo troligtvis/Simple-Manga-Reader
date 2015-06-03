c31ff761ad08224f4f20c21a4cdc290f23591ff8
strawberrypie <kjdr@kth.se>
2015-06-03 21:57:16 +0200

    Initial Commit

---
+++ Manga Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manga Reader
+//
+//  Created by Kj Drougge on 2015-06-03.
+//  Copyright (c) 2015 kj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
